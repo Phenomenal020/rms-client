79fd1287d13965812de7680630137d005f374d46
Phenomenal020 <stephenkalu2019@outlook.com>
2026-09-02 10:35:34 +0100

    fix newly discovered bugs

--- shadcn/ui/sidebar.tsx
+++ shadcn/ui/sidebar.tsx
@@ -5,7 +5,6 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { PanelLeftIcon } from "lucide-react"
 import { Slot } from "radix-ui"
 
-import { useIsMobile } from "@/hooks/use-mobile"
 import { cn } from "@/lib/utils"
 import { Button } from "@/shadcn/ui/button"
 import { Input } from "@/shadcn/ui/input"
@@ -66,7 +65,7 @@ function SidebarProvider({
   open?: boolean
   onOpenChange?: (open: boolean) => void
 }) {
-  const isMobile = useIsMobile()
+  const isMobile = false
   const [openMobile, setOpenMobile] = React.useState(false)
 
   // This is the internal state of the sidebar.
